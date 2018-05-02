89fe02621b76bc6e8588d6f25d77a2a5d3ae4c7e
SOSpacebar <ex-milobur@hotmail.com>
2018-05-02 12:06:04 +0800

    JJ Playground
    Testing

---
+++ Knovv It/Assets/Scripts/Input.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input : MonoBehaviour {
+
+    private TouchScreenKeyboard keyboard;
+	// Use this for initialization
+	void Start () {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
