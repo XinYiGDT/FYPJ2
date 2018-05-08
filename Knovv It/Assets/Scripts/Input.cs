203af47e7f4177aeacdd6ad15bc61612bae9de09
JJ\User <ex-milobur@hotmail.com>
2018-05-08 17:07:30 +0800

    Keyboard Testing

--- Knovv It/Assets/Scripts/Input.cs
+++ Knovv It/Assets/Scripts/Input.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Input : MonoBehaviour {
 
     private TouchScreenKeyboard keyboard;
 	// Use this for initialization
 	void Start () {
-        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+        //// Disable keyboard input space.
+        //TouchScreenKeyboard.hideInput = false;
+        //keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false);
+        ////keyboard.active = true;
 
+        //TouchScreenKeyboard keyboard;
+        TouchScreenKeyboard.hideInput = false;
+        if (keyboard == null || !TouchScreenKeyboard.visible)
+            keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false, "xyz");
     }
 	
 	// Update is called once per frame
