e217c9ae0d8f7d43e2a5dac2368f4ae108b50d71
XinYiGDT <kumiko-95@hotmail.com>
2018-05-17 00:30:09 +0800

    keyboard typeable

---
+++ Knovv It/Assets/Scripts/InputText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputText : MonoBehaviour {
+
+    string Word = null;
+    int WordIndex = 0;
+    string Alpha;
+    public Text myInput = null;
+
+	// Use this for initialization
+	public void alphabetFunction (string Alphabet)
+    {
+        WordIndex++;
+        Word = Word + Alphabet;
+        myInput.text = Word;
+	}
+
+	
+    public void delelteFunction ()
+    {
+
+    }
+}
