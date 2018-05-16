69ebb33ee9bc452e70a1139dadb3739611a5c77f
XinYiGDT <kumiko-95@hotmail.com>
2018-05-17 01:41:11 +0800

    done with keyboard

--- Knovv It/Assets/Scripts/InputText.cs
+++ Knovv It/Assets/Scripts/InputText.cs
@@ -6,21 +6,49 @@ using UnityEngine.UI;
 public class InputText : MonoBehaviour {
 
     string Word = null;
-    int WordIndex = 0;
-    string Alpha;
+    int WordIndex = -1;
+    string Alpha = null;
+    string Alpha2 = null;
+    char[] nameLetter = new char[30];
     public Text myInput = null;
 
 	// Use this for initialization
-	public void alphabetFunction (string Alphabet)
+	public void AlphabetFunction (string Alphabet)
     {
         WordIndex++;
-        Word = Word + Alphabet;
-        myInput.text = Word;
+        char[] keepLetter;
+        keepLetter = Alphabet.ToCharArray();
+        nameLetter[WordIndex] = keepLetter[0];
+        Alpha = nameLetter[WordIndex].ToString();
+        Word = Word + Alpha;
+        myInput.text = UppercaseFirst(Word);
 	}
 
 	
-    public void delelteFunction ()
+    public void DelelteFunction ()
     {
+        if (WordIndex >= 0)
+        {
+            WordIndex--;
 
+            Alpha2 = null;
+            for(int i=0; i<WordIndex +1; ++i)
+            {
+                Alpha2 = Alpha2 + nameLetter[i].ToString();
+            }
+            Word = Alpha2;
+            myInput.text = UppercaseFirst(Word);
+        }
+
+    }
+
+    static string UppercaseFirst(string s)
+    {
+        //check for empty string
+        if(string.IsNullOrEmpty(s))
+        {
+            return string.Empty;
+        }
+        return char.ToUpper(s[0]) + s.Substring(1);
     }
 }
