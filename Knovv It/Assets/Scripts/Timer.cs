8ae750d14745a45eb145796fb479eac048dd5a76
XinYiGDT <kumiko-95@hotmail.com>
2018-05-22 18:45:10 +0800

    -so called done with particle

--- Knovv It/Assets/Scripts/Timer.cs
+++ Knovv It/Assets/Scripts/Timer.cs
@@ -9,10 +9,14 @@ public class Timer : MonoBehaviour {
     public float maxTime = 5f;
     float timeLeft;
     public GameObject timesUp;
+    //public Transform pos;
+
+    public float timeStartDelay;
+    public float timeUpDelay;
 
 	// Use this for initialization
 	void Start () {
-        timesUp.gameObject.SetActive(false);
+        timesUp.SetActive(false);
         //timesUp.Pause();
         TimerBar = GetComponent<Image>();
         timeLeft = maxTime;
@@ -22,16 +26,32 @@ public class Timer : MonoBehaviour {
 	void Update () {
 		if(timeLeft > 0)
         {
-            timeLeft -= Time.deltaTime;
-            TimerBar.fillAmount = timeLeft / maxTime;
+            if (timeStartDelay > 0)
+            {
+                timeStartDelay -= Time.deltaTime;
+            }
+            else
+            {
+                timeStartDelay = 0;
+                timeLeft -= Time.deltaTime;
+                TimerBar.fillAmount = timeLeft / maxTime;
+            }
         }
         else
         {
-            timesUp.SetActive(true);
-            //ParticleSystem.EmissionModule module = timesUp.emission;
-            //module.enabled = true;
+            if (timeUpDelay > 0)
+            {
+                timeUpDelay -= Time.deltaTime;
+            }
+            else
+            { 
+                timesUp.SetActive(true);
+                Time.timeScale = 0;
+            }
+            // ParticleSystem TimesUpInstance = Instantiate(timesUp,pos);
             //timesUp.Play();
-            Time.timeScale = 0;
+            //Destroy(TimesUpInstance, timeSpawn);
+            
         }
 	}
 }
