4278b54241b0dd85bbf8752bc0ff8cdb58493819
XinYiGDT <kumiko-95@hotmail.com>
2018-05-25 21:54:41 +0800

    -changeable theme keyboard

---
+++ Knovv It/Assets/Scripts/SpriteChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteChange : MonoBehaviour {
+
+    public Sprite DefaultColor;
+    public Sprite BlackColor;
+
+    private Image GoSR;
+
+    // Use this for initialization
+    void Start () {
+        GoSR = this.gameObject.GetComponent<Image>();
+        GoSR.sprite = DefaultColor;
+    }
+	
+	public void ChangeColor () {
+            if (GoSR.sprite == DefaultColor)
+                GoSR.sprite = BlackColor;
+            else if (GoSR.sprite == BlackColor)
+                GoSR.sprite = DefaultColor;
+	}
+}
