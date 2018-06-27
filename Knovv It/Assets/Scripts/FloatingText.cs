95526ef53d5d5aedb870c8fa77ce8b782c5b9457
SIDM\163935T <kumiko-95@hotmail.com>
2018-06-27 11:33:50 +0800

    WIP popup text

---
+++ Knovv It/Assets/Scripts/FloatingText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingText : MonoBehaviour {
+    public Animator animator;
+    private Text popText;
+
+	void OnEnable ()
+    {
+        AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+        Destroy(gameObject, clipInfo[0].clip.length);
+        popText = animator.GetComponent<Text>();
+	}
+	
+	public void SetText(string text)
+    {
+        popText.text = text;
+    }
+}
