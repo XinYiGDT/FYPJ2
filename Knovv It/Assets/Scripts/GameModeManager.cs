4de52ce12feab60312bdb9273339eb0aba6926f2
JJ\User <ex-milobur@hotmail.com>
2018-08-06 02:14:31 +0800

    Updatesss

---
+++ Knovv It/Assets/Scripts/GameModeManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameMode
+{
+    Standard,
+    SuddenDeath,
+    Topic
+}
+
+
+public class GameModeManager : Singleton<GameModeManager> {
+
+    public GameMode currentGameMode;
+
+    //Some sort of gamemode base.
+
+}
---
+++ Knovv It/Assets/Scripts/Gamemode/GameModeBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class GameModeBase : MonoBehaviour {
+
+    abstract public void SetUp();
+    abstract public void TearDown();
+}
